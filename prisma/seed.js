cf4279ba9b279805328738c047337614fc80b845
Joel Watkins <eck087@yahoo.co.uk>
2023-01-16 12:03:38 +0000

    requirement 2- profile model and seed complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,16 +2,26 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-  const createdUsers = await prisma.user.createMany({
-    data: [
-      { username: "alicem", email: "alice@gmail.com" },
-      { username: "joelzor", email: "joel@yahoo.co.uk", isAdmin: true },
-    ],
+  const createdUser1 = await prisma.user.create({
+    data: { username: "alicem", email: "alice@gmail.com" },
   });
 
-  console.log(`${createdUsers.count} users created`, createdUsers);
+  const createdUser2 = await prisma.user.create({
+    data: { username: "joelzor", email: "joel@yahoo.co.uk", isAdmin: true },
+  });
 
   // Add your code here
+  const createdProfile = await prisma.profile.create({
+    data: {
+      profilePic: "http://fakeimages.com/joel-profile-pic",
+      bio: "creater of the joelzor bloggosphere!",
+      user: {
+        connect: {
+          id: createdUser2.id,
+        },
+      },
+    },
+  });
 
   // Don't edit any of the code below this line
   process.exit(0);
