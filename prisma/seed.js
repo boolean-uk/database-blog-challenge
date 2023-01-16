cdba31e97487e8e24857c4939c6d72104f14e3a9
Joel Watkins <eck087@yahoo.co.uk>
2023-01-16 15:26:31 +0000

    added Post model and seeds

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,14 +2,6 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-  //   const createdUser1 = await prisma.user.create({
-  //     data: { username: "alicem", email: "alice@gmail.com" },
-  //   });
-
-  //   const createdUser2 = await prisma.user.create({
-  //     data: { username: "joelzor", email: "joel@yahoo.co.uk", isAdmin: true },
-  //   });
-
   const createdUsers = await prisma.user.createMany({
     data: [
       { username: "alicem", email: "alice@gmail.com" },
@@ -22,6 +14,33 @@ async function seed() {
     data: {
       profilePic: "http://fakeimages.com/joel-profile-pic",
       bio: "creater of the joelzor bloggosphere!",
+      user: {
+        connect: {
+          id: 2,
+        },
+      },
+    },
+  });
+
+  const createdPost = await prisma.post.create({
+    data: {
+      title: "I had a terrible day",
+      body: "Hello folks. Wasn't my best day today. Let me tell you why in so many words... ",
+      published: true,
+      user: {
+        connect: {
+          id: 1,
+        },
+      },
+    },
+  });
+
+  const createdPost2 = await prisma.post.create({
+    data: {
+      title: "How to rule the world in 3 simple steps",
+      body: "In the following text, I will outline exactly how to rule the world financially, militarily and morally. Put down your phone and pay attention... ",
+      pictureURL: "http://worlddomination.net/throne",
+      published: false,
       user: {
         connect: {
           id: 2,
