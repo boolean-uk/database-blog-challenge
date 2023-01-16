d1077722b216661635d4659fb1739ae4ad268e34
Fatima <fxtimaahmed14@gmail.com>
2023-01-16 13:20:57 +0000

    working on req 3

--- prisma/seed.js
+++ prisma/seed.js
@@ -12,6 +12,20 @@ async function seed() {
   console.log(`${createdUsers.count} users created`, createdUsers);
 
   // Add your code here
+  const createdProfile = await prisma.profile.create({
+    data: [
+      {
+        user: {
+          connect: {
+            userId: createdUsers.id,
+          },
+        },
+        display_name: "alison the greatest",
+        profile_pic_url: "cat.jpeg",
+        bio: "I love to bake and spend time with my cat Gus",
+      },
+    ],
+  });
 
   // Don't edit any of the code below this line
   process.exit(0);
