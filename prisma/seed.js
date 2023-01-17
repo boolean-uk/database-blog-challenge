ef26a27cc1a4ef62512a8737fa17b23a60510dea
akibria12 <ahkibria@hotmail.co.uk>
2023-01-17 01:01:07 +0000

    core done

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,28 +1,55 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdUsers = await prisma.user.createMany({
-        data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
-    });
+  const createdUsers = await prisma.user.create({
+    data: { username: "alicemar10", email: "alice12@hotmail.co.uk" },
+  });
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
+  console.log("users created", createdUsers);
 
-    // Add your code here
+  // Add your code here
+  const createdProfile = await prisma.profile.create({
+    data: {
+      user: {
+        connect: { id: createdUsers.id },
+      },
+      profilePic: "flower",
+      bio: "this is my bio",
+    },
+  });
+  console.log("profile created", createdProfile);
 
-    
+  const createdPost = await prisma.post.create({
+    data: {
+      user: {
+        connect: { id: createdUsers.id },
+      },
+      title: "Arsenal is in the top of the table",
+      content: "Arsenal is looking for the title in the league",
+      isPublished: true,
+      picURL: "Arsenal team logo",
+    },
+  });
+  console.log("post created", createdPost);
 
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  const createComment = await prisma.comment.create({
+    data: {
+      user: {
+        connect: { id: createdUsers.id },
+      },
+      post: {
+        connect: { id: createdPost.id },
+      },
+      content: "Arsenal is 8 points ahead of man utd game this sunday",
+    },
+  });
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
