bf11979524b3692763ef4aca0958c786c2532c74
Fatima <fxtimaahmed14@gmail.com>
2023-01-17 10:14:25 +0000

    connect ids in Seed.js updated

--- prisma/seed.js
+++ prisma/seed.js
@@ -15,13 +15,21 @@ async function seed() {
   const createdProfile = await prisma.profile.createMany({
     data: [
       {
-        userId: 1,
+        user: {
+          connect: {
+            id: 1,
+          },
+        },
         display_name: "alison the greatest",
         profile_pic_url: "cat.jpeg",
         bio: "I love to bake and spend time with my cat Gus",
       },
       {
-        userId: 2,
+        user: {
+          connect: {
+            id: 2,
+          },
+        },
         display_name: "Martin Smith",
         profile_pic_url: "lake_pic.jpeg",
         bio: "Avid hiker + lover of lakes",
@@ -32,14 +40,22 @@ async function seed() {
   const createdPost = await prisma.post.createMany({
     data: [
       {
-        userId: 1,
+        user: {
+          connect: {
+            id: 1,
+          },
+        },
         title: "Gus the Ragdoll cat",
         content: "I love Gus, he is a 7 year old Ragdoll...blah blah blah",
         picture_url: "gus.jpeg",
         isPublished: true,
       },
       {
-        userId: 2,
+        user: {
+          connect: {
+            id: 2,
+          },
+        },
         title: "Chilling by a lake",
         content:
           "There is nothing like having a nice cup of tea whilst relaxing next to a lake.#blessd",
@@ -52,13 +68,29 @@ async function seed() {
   const createdComment = await prisma.comment.createMany({
     data: [
       {
-        userId: 1,
-        postId: 2,
+        user: {
+          connect: {
+            id: 1,
+          },
+        },
+        post: {
+          connect: {
+            id: 2,
+          },
+        },
         content: "loved this post so soooooooooooooooo much",
       },
       {
-        userId: 2,
-        postId: 1,
+        user: {
+          connect: {
+            id: 2,
+          },
+        },
+        post: {
+          connect: {
+            id: 1,
+          },
+        },
         content: "Gus looks adorable in this pic",
       },
     ],
