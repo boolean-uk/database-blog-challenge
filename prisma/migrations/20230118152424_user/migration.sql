ee5658bb23d8af094d84592ed0d51597aa404b27
Enky <enkh6at@yahoo.com>
2023-01-18 17:35:39 +0000

    core complete

---
+++ prisma/migrations/20230118152424_user/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "pictureURL" TEXT NOT NULL,
+    "bio" VARCHAR(120) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
--- prisma/seed.js
+++ prisma/seed.js
@@ -4,16 +4,57 @@ const prisma = new PrismaClient();
 async function seed() {
     const createdUsers = await prisma.user.createMany({
         data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
+            { username: 'alicemar', email: 'alice.martin@yahoo.com' },
+            { username: 'alice', email: 'alice.martina@hotmail.com' }
         ]
     });
 
     console.log(`${createdUsers.count} users created`, createdUsers);
 
     // Add your code here
+    const createdProfile = await prisma.profile.create({
+        data: {
+            pictureURL: "Alice in wonderland.",
+            bio: "Long time ago, far far away...",
+            user: {
+                connect: {id: 2}
+            }
+        }
+    })
 
-    
+    console.log(`${createdProfile.count} users created`, createdProfile);
+
+
+    const createdPost = await prisma.post.create({
+        data: {
+            title: "Alice in real life",
+            content: "She is now over 80 years of age, and in wonderland.",
+            published: true,
+            user: {
+                connect: {
+                    id: 2
+                }
+            }
+        }
+    })
+
+    console.log(`${createdPost.count} users created`, createdPost);
+
+    const createdComment = await prisma.comment.create({
+        data: {
+            content: "No that is lie she must be same as auther created Alice in Wonderland!",
+            user: {
+                connect: {
+                    id: 2
+                }
+            },
+            post: {
+                connect: {
+                    id: 1
+                }
+            }
+        }
+    })
 
 
     // Don't edit any of the code below this line
