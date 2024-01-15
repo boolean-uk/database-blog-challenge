1d35a9c09a2d39f15073ca21e2e26102b2368440
ilham saleh <ilham2saleh@gmail.com>
2024-01-15 16:03:09 +0000

    Complete ERD and update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,28 +1,33 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-async function seed() {
-    const createdUsers = await prisma.user.createMany({
-        data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
-    });
+const createdUsers = async function seed() {
+  await prisma.user.createMany({
+    data: [
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
+      {
+        username: "Alicemartin",
+        email: "alice@mail.com",
+        age: 27,
+      },
+      {
+        username: "John",
+        email: "john@gmail.com",
+        age: 25,
+      },
+    ],
+  });
 
-    // Add your code here
+  console.log(`${createdUsers.count} users created`, createdUsers);
 
-    
+  // Add your code here
 
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
