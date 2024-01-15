f882d46f64d32ecd78934a44086b260f157baf3f
ilham saleh <ilham2saleh@gmail.com>
2024-01-15 17:19:09 +0000

    Create Post model

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,36 +2,47 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-  await prisma.user.createMany({
+  const users = await prisma.user.createMany({
     data: [
-      {
-        username: "alicemartin",
-        email: "alice@mail.com",
-        age: 27,
-      },
-      {
-        username: "john",
-        email: "john@gmail.com",
-        age: 25,
-      },
+      { username: "alicemartin", email: "alice@mail.com", age: 27 },
+      { username: "john", email: "john@gmail.com", age: 25 },
+      { username: "jerry", email: "jerry@gmail.com", age: 33 },
+      { username: "tom", email: "tom@gmail.com", age: 35 },
     ],
   });
 
-  await prisma.profile.create({
-    data: {
+  const profilesData = [
+    {
+      profilePicture: "some_image_link",
+      bio: "some bio about the profile",
+      firstName: "alice",
+      lastName: "martin",
+      userId: users[0].id,
+    },
+    {
       profilePicture: "some_image_link",
       bio: "some bio about the profile",
       firstName: "john",
       lastName: "doe",
-      user: {
-        connect: {
-          id: 2,
-        },
-      },
+      userId: users[1].id,
     },
-  });
+    {
+      profilePicture: "some_image_link",
+      bio: "some bio about the profile",
+      firstName: "jerry",
+      lastName: "mouce",
+      userId: users[2].id,
+    },
+    {
+      profilePicture: "some_image_link",
+      bio: "some bio about the profile",
+      firstName: "tom",
+      lastName: "cat",
+      userId: users[3].id,
+    },
+  ];
 
-  // Add your code here
+  await prisma.profile.createMany({ data: profilesData });
 
   // Don't edit any of the code below this line
   process.exit(0);
