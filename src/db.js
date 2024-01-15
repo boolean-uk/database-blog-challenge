3bbf0e220ee6d17ce5d88be7e48b94dfb222e679
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-15 18:41:51 +0000

    feat: get all users

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
-const prisma = require("./index")
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
 
-const main =  async () => {
-}
-main() 
+module.exports = prisma
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-const { PrismaClient } = require("@prisma/client")
-const prisma = new PrismaClient()
+const prisma = require("./db");
 
-module.exports prisma
+const main = async () => {
+  const getAllUsers = await prisma.user.findMany();
+  console.log(getAllUsers);
+  // Get all posts that belong to the user with ID 2
+  // Get the user with ID 1 and include their profile in the response
+  // Update the post with ID 1 so that its text/content is different from what was created in the seed file
+  // Delete the post with ID 3
+};
+main();
