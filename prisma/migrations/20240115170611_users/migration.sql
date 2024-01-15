8ebb3800e4e06ca683bb83ec47f717418c92ea7a
Lukas Dembicki <lad_777@hotmail.com>
2024-01-15 20:42:49 +0000

    ADD: All requirement queries. CORE DONE.

---
+++ prisma/migrations/20240115170611_users/migration.sql
@@ -0,0 +1,83 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `username` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(10)`.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT,
+ALTER COLUMN "username" SET DATA TYPE VARCHAR(10);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "bio" VARCHAR(120) NOT NULL,
+    "profilePic" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(150) NOT NULL,
+    "content" TEXT NOT NULL,
+    "picture" TEXT,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "content" VARCHAR(250) NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_userId_key" ON "Post"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Comment_postId_key" ON "Comment"("postId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Comment_userId_key" ON "Comment"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const prisma = require("./db.js");
+
+async function getAllUsers() {
+    const getAllUsersDb = await prisma.user.findMany();
+    console.log(getAllUsersDb);
+}
+
+async function getPosts() {
+    const getPostsFromUsersWithId2 = await prisma.post.findMany({
+        where: {
+            userId: 2,
+        },
+    });
+    console.log(getPostsFromUsersWithId2);
+}
+
+async function getUser1WithProfile() {
+    const getUserOneWithProfile = await prisma.user.findUnique({
+        where: {
+            id: 1,
+        },
+        include: {
+            profile: true,
+        },
+    });
+    console.log(getUserOneWithProfile);
+}
+
+async function UpdatePost() {
+    const updatePostWithIdOne = await prisma.post.update({
+        where: {
+            id: 1,
+        },
+        data: {
+            content: "Hey, this is a changed post!",
+        },
+    });
+    console.log(updatePostWithIdOne);
+}
+
+async function deletePost() {
+    const deletePostWithId3 = await prisma.post.delete({
+        where: {
+            id: 3,
+        },
+    });
+    console.log(deletePostWithId3);
+}
+
+// getAllUsers()
+// getPosts()
+// getUser1WithProfile()
+// UpdatePost()
+deletePost();
