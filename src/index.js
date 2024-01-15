6dcdc98b1ffe080a5df94b4535ee82b694839c75
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-15 18:52:53 +0000

    feat: update the content of post with id : 1

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ const prisma = require("./db");
 const main = async () => {
   const getAllUsers = await prisma.user.findMany();
   const getAllPostForUserTwo = await prisma.post.findMany({
-    where: {
-      userId: 2,
-    }, 
+    where: { userId: 2 },
     include: {
-      user: {select: {username: true} }
-    }
+      user: { select: { username: true } },
+    },
   });
   const getUserOne = await prisma.user.findUnique({
-    where: {
-        id: 1
-    }, 
-    include: {profile: true}
-  })
-  console.log(getUserOne)
-  // Get the user with ID 1 and include their profile in the response
+    where: { id: 1 },
+    include: { profile: true },
+  });
+  const updatePostOne = await prisma.post.update({
+    where: { id: 1 },
+    data: {
+      content: "new text",
+    },
+  });
   // Update the post with ID 1 so that its text/content is different from what was created in the seed file
   // Delete the post with ID 3
 };
