7d6caf1da875f18cb116dc460a354954e57450a2
callumhayden <callum.hayden@gmail.com>
2024-01-15 14:28:19 +0000

    requirement 2 completed

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,8 +4,8 @@ const prisma = new PrismaClient();
 async function seed() {
     const createdUsers = await prisma.user.createMany({
         data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
+            { username: 'alice10', email: 'alice@example.com' },
+            { username: 'bobsmith', email: 'bob@example.com' }
         ]
     });
 
