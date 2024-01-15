0be254fbd8b2bded769229b559e495808d22e84b
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-15 18:48:51 +0000

    feat: get user with id : 1 with their profile

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ const main = async () => {
       user: {select: {username: true} }
     }
   });
+  const getUserOne = await prisma.user.findUnique({
+    where: {
+        id: 1
+    }, 
+    include: {profile: true}
+  })
+  console.log(getUserOne)
   // Get the user with ID 1 and include their profile in the response
   // Update the post with ID 1 so that its text/content is different from what was created in the seed file
   // Delete the post with ID 3
