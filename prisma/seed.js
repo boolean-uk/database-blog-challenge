3d4faabaaa81374ff690cc443e7815c53ab86456
callumhayden <callum.hayden@gmail.com>
2024-01-15 15:06:54 +0000

    requirements 5 completed

--- prisma/seed.js
+++ prisma/seed.js
@@ -31,22 +31,38 @@ async function seed() {
   console.log(`${createdProfile.count} profile created`, createdProfile);
 
   const createdPost = await prisma.post.createMany({
-    data:[
+    data: [
+      {
+        title: "i love code",
+        content: "this is my new post",
+        authorId: 1,
+      },
+      {
+        title: "i hate code",
+        content: "this is my last post",
+        authorId: 2,
+      },
+    ],
+  });
+
+  console.log(`${createdPost.count} post created`, createdPost);
+
+  const createdComment = await prisma.comment.createMany({
+    data: [
         {
-            title: "i love code",
-            content: "this is my new post",
-            authorId: 1
+            text: "i love coding javascript",
+            postId: 1,
+            authorId: 1,
         },
-        {
-            title: "i hate code",
-            content: "this is my last post",
-            authorId: 2
-        }
-    ]
-  })
+      {
+        text: "i hate coding javascript",
+        postId: 2,
+        authorId: 2,
+      }
+    ],
+  });
 
-  console.log(`${createdPost.count} post created`, createdPost)
-
+  console.log(`${createdComment.count} comment created`, createdComment)
   // Don't edit any of the code below this line
   process.exit(0);
 }
