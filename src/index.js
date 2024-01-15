0c8e1b5beb18494e7a5f9b150fa4faef5d09e15e
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-15 18:46:27 +0000

    feat: get the posts with user id : 2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const prisma = require("./db");
 
 const main = async () => {
   const getAllUsers = await prisma.user.findMany();
-  console.log(getAllUsers);
-  // Get all posts that belong to the user with ID 2
+  const getAllPostForUserTwo = await prisma.post.findMany({
+    where: {
+      userId: 2,
+    }, 
+    include: {
+      user: {select: {username: true} }
+    }
+  });
   // Get the user with ID 1 and include their profile in the response
   // Update the post with ID 1 so that its text/content is different from what was created in the seed file
   // Delete the post with ID 3
