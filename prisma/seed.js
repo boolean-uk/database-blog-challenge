1c2119528a34e82e4dce155de98b3bf9c1f13ce2
Faiza <faraz_ul_islam@yahoo.com>
2024-01-16 20:51:47 +0000

    post model added

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,12 +4,86 @@ const prisma = new PrismaClient();
 async function seed() {
     const createdUsers = await prisma.user.createMany({
         data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
-    });
+            {
+              username: 'Faiza',
+              city: 'London',
+              email: 'faiza@gmail.com'
+            },
+            {
+              username: 'Sara',
+              city: 'Luton',
+              email: 'sara@gmail.com'
+            },
+            {
+              username: 'Alice',
+              city: 'Bradford',
+              email: 'alice@gmail.com'
+            }
+          ]
+        });
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
+        console.log(`${createdUsers.count} users created`, createdUsers);
+
+
+
+          const createProfile1 = await prisma.profile.create({
+            data: {
+              biography: 'Biography of Faiza',
+              picture_url: 'picture url for profile',
+              user: {
+                connect: {
+                  id: 1
+                }
+              }
+            }
+          })
+        
+          const createProfile2 = await prisma.profile.create({
+            data: {
+              biography: 'Biography of Sara',
+              picture_url: 'picture url for profile',
+              user: {
+                connect: {
+                  id: 2
+                }
+              }
+            }
+          })
+        
+          const createProfile3 = await prisma.profile.create({
+            data: {
+              biography: 'Biography of Alice',
+              picture_url: 'picture url for profile',
+              user: {
+                connect: {
+                  id: 3
+                }
+              }
+            }
+          })
+        
+          console.log('Created profiles', [
+            createProfile1,
+            createProfile2,
+            createProfile3
+        ])
+
+
+
+
+        const createPost = await prisma.post.create({
+            data: {
+              title: 'Post of Sara',
+              content: 'Sara post content',
+              picture_URL: 'Sara picture_URL',
+              is_publish: true,
+              profileId: 3
+            },
+          });
+         
+          console.log('Created post of user 2', createPost);
+          
+  
 
     // Add your code here
 
