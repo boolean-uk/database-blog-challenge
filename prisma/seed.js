dc54c465a65ae0c5b39a9c84dc24087b243ee3d6
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-01-16 14:07:35 +0100

    requiment for user

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,28 +1,34 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdUsers = await prisma.user.createMany({
-        data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
-    });
+  const createdUsers = await prisma.user.createMany({
+    data: [
+      { username: "alicemartin", email: "alicemartin@gmail.com" },
+      { username: "alice", email: "alice@gmail.com" },
+    ],
+  });
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
+  console.log(`${createdUsers.count} users created`, createdUsers);
 
-    // Add your code here
+  // Add your code here
 
-    
+  const theCreatedUser = await Prisma.user.create({
+    data: {
+      username: "Hayor4real",
+      email: "hayor4real@gmail.com",
+      password: "hayorinde456",
+    },
+  });
 
+  console.log("user created", theCreatedUser);
 
-    // Don't edit any of the code below this line
-    process.exit(0);
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
