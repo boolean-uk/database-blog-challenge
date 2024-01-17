769ad834c8c9f098f9740a7ec415cf439d8c09b8
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-17 23:01:20 +0100

    added query to get posts of userId 2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ const prisma = require("./db.js");
 
 async function main() {
   try {
+    //to get all the users
     const getAllUsers = await prisma.user.findMany();
     console.log("all users", getAllUsers);
+
+    //get all the post of userId 2
+    const getPost = await prisma.post.findMany({
+      where: {
+        userId: 2,
+      },
+    });
+
+    console.log("posts of user ID 2", getPost);
   } catch (error) {
     console.log("Error executing queries", error);
   }
