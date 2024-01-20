ef5e725cb71d34e9dfc142c3a826b8711e828ca0
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-01-20 08:23:31 -0800

    getPostById

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ async function main() {
 
     const getAllUsers = await prisma.user.findMany()
     console.log(getAllUsers)
+    
+
+    const getPostById = await prisma.post.findMany({
+        where: {userId: 2},
+    })
+    console.log('posts of userId 2',getPostById)
 }
 
 main()
