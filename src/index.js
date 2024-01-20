c8c3324c35b043805cd7266abf64c575784b515d
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-01-20 08:39:40 -0800

    getUser and include profile

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ async function main() {
 
     const getAllUsers = await prisma.user.findMany()
     console.log(getAllUsers)
-    
+
 
     const getPostById = await prisma.post.findMany({
         where: {userId: 2},
     })
     console.log('posts of userId 2',getPostById)
+    
+
+    const getUser = await prisma.user.findMany({
+        where: {
+            id: 1
+        },
+        include: {
+            profile: true
+        }
+    })
+    console.log('--------',getUser)
 }
 
 main()
