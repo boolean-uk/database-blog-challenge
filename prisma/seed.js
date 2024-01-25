4c43cc0a17995d4df243edd6b6b57dba562af1e0
Faiza <faraz_ul_islam@yahoo.com>
2024-01-25 11:14:08 +0000

    updated model user relationship

--- prisma/seed.js
+++ prisma/seed.js
@@ -22,26 +22,30 @@ async function seed() {
     ]
   });
 
-  console.log(`${createdUsers.count} users created`, createdUsers);
+  //console.log(`${createdUsers.count} users created`, createdUsers);
+  console.log(`${Object.keys(createdUsers).length} users created`, createdUsers);
+
 
 
 
   const createProfile = await prisma.profile.createMany({
     data: [
       {
-        id: 1,
+        user_id : 1,
         biography: 'Biography of Faiza',
         picture_url: 'picture url for profile',
       },
       {
+        user_id : 2,
         biography: 'Biography of Sara',
         picture_url: 'picture url for profile',
       },
       {
+        user_id : 3,
         biography: 'Biography of Alice',
         picture_url: 'picture url for profile',
       },
-    ],
+    ]
   });
 
 
@@ -51,45 +55,45 @@ async function seed() {
       content: 'Sara post content',
       picture_URL: 'Sara picture_URL',
       is_publish: true,
-      user_Id: 3
+      profileId: 3
     },
        {
       title: "Studying with us",
       content: "Come join study with us",
       isPublished: true,
-      user_Id: 3,
+      profileId: 3,
     },
     {
       title: "Travelling",
       content: "Travel  to the Malaysia",
       picture_URL: ' picture_URL',
       isPublished: true,
-      user_Id: 1,
+      profileId: 1,
     },
     {
       title: "Cooking",
       content: "Cooking Biryani",
       picture_URL: ' picture_URL',
       isPublished: true,
-      user_Id: 1,
+      profileId: 1,
     },
     {
       title: "Writting",
       content: "Novel Writting",
       isPublished: true,
-      user_Id: 2,
+      profileId: 2,
     },
   ]
     });
 
-console.log('Created post of user_id', CreatePost);
+console.log('Created post of user_id', createPost);
 
 
 const createComment = await prisma.comment.createMany({
   data: [
-    { content: "work work work", userId: 1, postId: 1 },
-    { content: "Nice work", userId: 2, postId: 2 },
-    { content: "job done", userId: 3, postId: 3 },
+    { content: "work work work",  user_id: 1, postId: 1 },
+    { content: "Nice work",  user_id: 2, postId: 2 },
+    { content: "job done",  user_id: 3, postId: 3 },
   ],
 });
 
